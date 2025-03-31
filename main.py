f1400f4ee164cc7f462ddc7a8146de819023c7f2
Jack Fallows <iamjfallows@gmail.com>
2025-03-31 18:02:55 -0400

    danger table implementation

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
+import os
 import random
+import sys
 import time
-import gymnasium
+import gymnasium as gym
+
+# Add coverage-gridworld to path
+sys.path.append("./coverage-gridworld")
+print(sys.path)
+
 import coverage_gridworld  # must be imported, even though it's not directly referenced
 
+# Import environment registration
+# from custom import observation_space
+
+# New shit
+def determine_cell_danger_tables(env, enemies):
+    # Take in location of enemies in particular
+    danger_tables = [[[0 for _ in range(4)] for _ in range(10)] for _ in range(10)]
+    for e in enemies:
+        orientation = e.orientation
+        # Try all 4 orientations counter clockwise
+        for i in range(4):
+            match orientation:
+                case 0:
+                    # Assume enemies can see 4 in any direction
+                    for j in range(1, 5):
+                        if e.x - j < 0: break
+                        danger_tables[e.x - j][e.y][i] = 1
+                case 1:
+                    for j in range(1, 5):
+                        if e.y + j > 9: break
+                        danger_tables[e.x][e.y + j][i] = 1
+                case 2:
+                    for j in range(1, 5):
+                        if e.x + j > 9: break
+                        danger_tables[e.x + j][e.y][i] = 1
+                case 3:
+                    for j in range(1, 5):
+                        if e.y - j < 0: break
+                        danger_tables[e.x][e.y - j][i] = 1
+            orientation = (orientation + 1) % 4
+    return danger_tables
 
 def human_player():
     # Write the letter for the desired movement in the terminal/console and then press Enter
@@ -23,6 +61,9 @@ def human_player():
 
 
 def random_player():
+    return random.randint(0, 4)
+
+def rl_player():
     return random.randint(0, 4)
 
 
@@ -89,16 +130,20 @@ maps = [
     ]
 ]
 
-env = gymnasium.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
-num_episodes = 5
+env = gym.make("sneaky_enemies", render_mode="human", predefined_map_list=None, activate_game_status=True)
+
+num_episodes = 1
 
 for i in range(num_episodes):
     env.reset()
     done = False
-    while not done:
-        action = random_player()
+    # Do nothing for one step, just to get danger table values
+    obs, reward, done, truncated, info = env.step(4)
+    danger_table = determine_cell_danger_tables(env, info["enemies"])
+    time.sleep(1)
+    while done is False:
+        action = rl_player()
         obs, reward, done, truncated, info = env.step(action)
-
         # Sleep may be used to allow each step to be visualized. Value can be changed
         #time.sleep(0.2)
     if done:
